77bcfcd0976edb0fecc6a53d02cbf5c25d4b83ca
aryananilagrawal-source <aryananilagrawal@gmail.com>
2026-09-14 02:45:07 +0530

    Add 5 assignment questions for week 2

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixMap = new HashMap<>();
+        prefixMap.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+            if (prefixMap.containsKey(currentSum - k)) {
+                count += prefixMap.get(currentSum - k);
+            }
+            prefixMap.put(currentSum, prefixMap.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subarraySum(new int[]{1, 1, 1}, 2));
+        System.out.println(subarraySum(new int[]{1, -1, 0}, 0));
+    }
+}
